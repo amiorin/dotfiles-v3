a48aadbce0e7c39d2d1fe727a5904e9eb87e668c
Alberto Miorin <32617+amiorin@users.noreply.github.com>
2025-09-10 12:02:27 +0200

    refactor: repos

--- src/amiorin/repos.clj
+++ src/amiorin/repos.clj
@@ -3,39 +3,23 @@
    [clojure.java.io :as io]
    [selmer.parser :as p]))
 
-(def default-repos [{:user "vscode"
-                     :org "amiorin"
-                     :repo "dotfiles-v3"
-                     :branch "main"
-                     :worktrees ["minipc"
-                                 "ansible"]}
-                    {:user "vscode"
-                     :org "amiorin"
-                     :repo "albertomiorin.com"
-                     :branch "main"
-                     :worktrees ["albertomiorin"
-                                 "big-config"]}
-                    {:user "vscode"
-                     :org "amiorin"
-                     :repo "big-config"
-                     :branch "main"
-                     :worktrees ["deps-new"]}
-                    {:user "vscode"
-                     :org "amiorin"
-                     :repo "big-container"
-                     :branch "main"
-                     :worktrees []}
-                    {:user "vscode"
-                     :org "amiorin"
-                     :repo "rama-jdbc"
-                     :branch "main"
-                     :worktrees []}
-                    {:user "alberto"
-                     :org "amiorin"
-                     :repo "dotfiles-v3"
-                     :branch "main"
-                     :worktrees ["minipc"
-                                 "ansible"]}])
+(def default-repos
+  (-> (into [] (for [[repo worktrees] [["dotfiles-v3" ["minipc" "ansible"]]
+                                       ["albertomiorin.com" ["albertomiorin" "big-config"]]
+                                       ["big-config" ["deps-new"]]
+                                       ["big-container" []]
+                                       ["rama-jdbc" []]]]
+                 {:user "vscode"
+                  :org "amiorin"
+                  :repo repo
+                  :branch "main"
+                  :worktrees worktrees}))
+      (into [{:user "alberto"
+              :org "amiorin"
+              :repo "dotfiles-v3"
+              :branch "main"
+              :worktrees ["minipc"
+                          "ansible"]}])))
 
 (defn render
   [s repos]
