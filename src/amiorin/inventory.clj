e4ee91bb977c95b422c36e05ef799c81e8c84e0d
Alberto <32617+amiorin@users.noreply.github.com>
2025-09-13 17:01:29 +0000

    feat: pin different versions of emacs

--- src/amiorin/inventory.clj
+++ src/amiorin/inventory.clj
@@ -6,12 +6,15 @@
   []
   [{:name "vscode"
     :uid "1001"
+    :doomemacs "68010af0906171e3c989fc19bcb3ba81f7305022"
     :remove false}
    {:name "alberto"
     :uid "1002"
+    :doomemacs "68010af0906171e3c989fc19bcb3ba81f7305022"
     :remove false}
    {:name "amiorin"
     :uid "1003"
+    :doomemacs "d6cdbb4d22a2db68e3d74b7f5abf5b1fd14f4de0"
     :remove false}])
 
 (defn render
