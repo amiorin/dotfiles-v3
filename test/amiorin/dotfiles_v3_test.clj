8a839078d476a83728c8d5b2a1ab433f8780d278
Alberto <32617+amiorin@users.noreply.github.com>
2025-09-13 07:27:10 +0000

    feat: add unit test

--- test/amiorin/dotfiles_v3_test.clj
+++ test/amiorin/dotfiles_v3_test.clj
@@ -1,7 +1,6 @@
 (ns amiorin.dotfiles-v3-test
   (:require
    [amiorin.dotfiles-v3 :refer [run-steps]]
-   [babashka.fs :as fs]
    [babashka.process :refer [shell]]
    [big-config :as bc]
    [clojure.edn :as edn]
@@ -17,13 +16,19 @@
       (is (s/valid? :org.corfield.new/template template)
           (s/explain-str :org.corfield.new/template template)))))
 
+(defn discover
+  []
+  (-> (shell {:out :string} "find dist -mindepth 2 -maxdepth 2 -type d")
+      :out
+      str/trim
+      (str/split #"\n")
+      (->> (map #(str/split % #"/"))
+           (map rest))))
+
 (deftest stability
   (testing "working directory is clean after running all modules"
-    (let [modules (->> (fs/list-dir "dist/prod")
-                       (map str)
-                       (map #(str/replace-first % "dist/prod/" "")))]
-      (doseq [module modules]
-        (run-steps (format "build -- %s prod" module) [] {::bc/env :repl}))
-      (as-> (shell {:continue true} "git diff --quiet") $
-        (:exit $)
-        (is (= $ 0) "The working directory is not clean")))))
+    (doseq [[module profile] (discover)]
+      (run-steps (format "build -- %s %s" module profile) [] {::bc/env :repl}))
+    (as-> (shell {:continue true} "git diff --quiet") $
+      (:exit $)
+      (is (= $ 0) "The working directory is not clean"))))
