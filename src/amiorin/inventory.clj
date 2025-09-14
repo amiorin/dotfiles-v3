6ce03587690842e73de43d1c25afa3934eaf1bda
Alberto Miorin <32617+amiorin@users.noreply.github.com>
2025-09-14 18:28:16 +0200

    feat: multi host

--- src/amiorin/inventory.clj
+++ src/amiorin/inventory.clj
@@ -21,19 +21,29 @@
     :doomemacs "d6cdbb4d22a2db68e3d74b7f5abf5b1fd14f4de0"
     :remove false}])
 
-(defn render
-  [users]
-  (let [users (filter (complement :remove) users)
-        host "minipc"
-        admin "alberto"
-        hosts (reduce #(let [{:keys [name uid]} %2]
-                         (assoc %1 name {:ansible_host host
-                                         :ansible_user name
-                                         :uid uid})) {} users)
-        res {:all {:children {:admin {:hosts {:root {:ansible_host host
-                                                     :ansible_user admin}}}
-                              :users {:hosts hosts}}}}]
-    (generate-string res {:pretty true})))
+  (defn render
+    [users]
+    (let [sudoer "ubuntu"
+          users (-> (filter (complement :remove) users)
+                    (->> (map #(for [host ["minipc" "soyo"]]
+                                 (assoc % :host host))))
+                    flatten)
+          admins (-> [{:ansible_user sudoer}]
+                     (->> (map #(for [host ["minipc" "soyo"]]
+                                  (-> %
+                                      (merge {:host host
+                                              :name "ubuntu"})))))
+                     flatten)
+          hosts-users (reduce #(let [{:keys [name uid host]} %2]
+                                 (assoc %1 (format "%s@%s" name host) {:ansible_host host
+                                                                       :ansible_user name
+                                                                       :uid uid})) {} users)
+          hosts-admins (reduce #(let [{:keys [name host]} %2]
+                                  (assoc %1 (format "root@%s" host) {:ansible_host host
+                                                                     :ansible_user name})) {} admins)
+          res {:all {:children {:admin {:hosts hosts-admins}
+                                :users {:hosts hosts-users}}}}]
+      (generate-string res {:pretty true})))
 
 (comment
   (render (default-opts)))
