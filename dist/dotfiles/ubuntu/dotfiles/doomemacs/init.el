e459dc0e20f711cc11563a0d8ecd1e25a82d4877
Alberto <32617+amiorin@users.noreply.github.com>
2025-09-17 08:13:24 +0000

    fix: centered-window repo error

--- dist/dotfiles/ubuntu/dotfiles/doomemacs/init.el
+++ dist/dotfiles/ubuntu/dotfiles/doomemacs/init.el
@@ -138,7 +138,7 @@
        ;;nim               ; python + lisp at the speed of c
        (nix +lsp +tree-sitter)               ; I hereby declare "nix geht mehr!"
        ;;ocaml             ; an objective camel
-       (org +present +dragndrop)               ; organize your plain life in plain text
+       org               ; organize your plain life in plain text
        ;;php               ; perl's insecure younger brother
        ;;plantuml          ; diagrams for confusing people more
        ;;graphviz          ; diagrams for confusing yourself even more
