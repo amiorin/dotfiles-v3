24b0ed11856557c7168f270a90045075979857f2
Alberto Miorin <32617+amiorin@users.noreply.github.com>
2025-09-27 05:50:01 +0200

    bump: big-config

--- src/dotfiles.clj
+++ src/dotfiles.clj
@@ -6,28 +6,29 @@
    [big-config.step-fns :as step-fns]))
 
 (defn run-steps [s opts]
-  (let [opts (merge opts {::build/recipes [{:template "amiorin/dotfiles_v3"
-                                            :data-fn (fn [{:keys [::step/module ::step/profile]} _]
-                                                       {:module module
-                                                        :profile profile})
-                                            :template-fn (fn [{:keys [::step/module ::step/profile]} edn]
-                                                           (assoc edn :target-dir (format "dist/%s/%s" module profile)))
-                                            :overwrite :delete
-                                            :transform [["build"
-                                                         :raw]
-                                                        ["{{ profile }}"
-                                                         :raw]]}]})
+  (let [opts (merge opts {::build/templates [{:template "stage-1"
+                                              :data-fn (fn [{:keys [::step/profile]} _]
+                                                         {:profile profile})
+                                              :template-fn (fn [{:keys [:profile]} edn]
+                                                             (assoc edn :target-dir (format "resources/stage-2/%s" profile)))
+                                              :overwrite :delete
+                                              :transform [["build"
+                                                           :raw]
+                                                          ["{{ profile }}"
+                                                           :raw]]}]})
         step-fns [step/print-step-fn
                   (step-fns/->exit-step-fn ::step/end)
                   (step-fns/->print-error-step-fn ::step/end)]]
     (step/run-steps s step-fns opts)))
 
 (comment
-  (build/create {::build/recipes [{:template "amiorin/dotfiles_v3"
-                                   :target-dir "dist/dotfiles/macos"
-                                   :overwrite :delete
-                                   :transform [["build"
-                                                :raw]
-                                               ["ubuntu"
-                                                :raw]]}]})
+  (build/create {::build/templates [{:template "stage-1"
+                                     :target-dir "resources/stage-2/ubuntu"
+                                     :post-process-fn (fn [edn data]
+                                                        (println "foo"))
+                                     :overwrite :delete
+                                     :transform [["build"
+                                                  :raw]
+                                                 ["ubuntu"
+                                                  :raw]]}]})
   (run-steps "create -- dotfiles macos" {::bc/env :repl}))
