91d28d48252664513b1fcfa8a1a25c6af1dccbdf
Alberto Miorin <32617+amiorin@users.noreply.github.com>
2026-02-15 10:40:39 +0100

    feat: add vscode to 1001

--- src/hetzner.clj
+++ src/hetzner.clj
@@ -3,31 +3,38 @@
 (defn data-fn [_ _]
   (let [sudoer "root"
         main-user "ubuntu"
-        hosts ["95.217.164.175" "46.62.162.129"]
+        hosts ["95.217.164.175"]
         users [{:name main-user
                 :uid "1000"
+                :doomemacs "6ea4332b854d311d7ec8ae6384fae8d9871f5730"
+                :remove false}
+               {:name "vscode"
+                :uid "1001"
                 :doomemacs "6ea4332b854d311d7ec8ae6384fae8d9871f5730"
                 :remove false}]
         config {:users (filter (complement :remove) users)
                 :remove_users (filter :remove users)
                 :atuin_login "{{ lookup('ansible.builtin.env', 'ATUIN_LOGIN') }}"
                 :ssh_key "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIHDKdUkY+SfRm6ttOz2EEZ2+i/zm+o1mpMOdMeGUr0t4 32617+amiorin@users.noreply.github.com"}
-        repos (-> (into [] (for [[repo worktrees] [["dotfiles-v3" ["ansible" "babashka"]]
-                                                   ["albertomiorin.com" ["albertomiorin" "big-config"]]
-                                                   ["big-config" ["deps-new" "hyperlith" "hyperlith-counter"]]
-                                                   ["big-container" []]
-                                                   ["rama-jdbc" []]]]
-                             {:user main-user
-                              :org "amiorin"
-                              :repo repo
-                              :branch "main"
-                              :worktrees worktrees}))
-                  (into (for [[repo worktrees] [["hyperlith" []]]]
-                          {:user main-user
-                           :org "amiorin"
-                           :repo repo
-                           :branch "master"
-                           :worktrees worktrees})))
+        repos (-> (into [] (for [user [main-user "vscode"]]
+                             (for [[repo worktrees] [["dotfiles-v3" ["ansible" "babashka"]]
+                                                     ["albertomiorin.com" ["albertomiorin" "big-config"]]
+                                                     ["big-config" ["deps-new" "hyperlith" "hyperlith-counter"]]
+                                                     ["big-container" []]
+                                                     ["rama-jdbc" []]]]
+                               {:user user
+                                :org "amiorin"
+                                :repo repo
+                                :branch "main"
+                                :worktrees worktrees})))
+                  (into (for [user [main-user "vscode"]]
+                          (for [[repo worktrees] [["hyperlith" []]]]
+                            {:user user
+                             :org "amiorin"
+                             :repo repo
+                             :branch "master"
+                             :worktrees worktrees})))
+                  flatten)
         packages (->> ["fish"
                        "emacs"
                        "zellij"
